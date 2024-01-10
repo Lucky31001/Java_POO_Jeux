6bc2c325f27265071a9e78da423688d7587f5c07
Kwak <kilian.dumaire@gmail.com>
2024-01-10 15:54:55 +0100

    Realisation d'un p'tit systheme de combat

--- POO_Java/src/Main.java
+++ POO_Java/src/Main.java
@@ -1,5 +1,14 @@
+import Models.Characters.Characters;
+import Models.Characters.Enemies;
+import Models.Characters.Player;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worlds");
+        Characters Perso = new Player(100, 30, 50, 50, "McAdam", 50);
+        Characters Ennemis = new Enemies(0, 0, 0, 0, "McZam", 0, 1);
+
+        while (Ennemis.getHp() > 0) {
+            Perso.attack(Ennemis);
+        }
     }
 }
