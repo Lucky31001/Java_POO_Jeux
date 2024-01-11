470c4acf7e8c516f442be97db8c6cb863f8a6ad7
ouachtou <pierre.legrand1105@gmail.com>
2024-01-11 13:16:20 +0100

    cli

--- POO_Java/src/Main.java
+++ POO_Java/src/Main.java
@@ -5,9 +5,11 @@ import Models.Objects.Consumable;
 
 public class Main {
     public static void main(String[] args) {
-        Characters Perso = new Player(100, 200, 50, 50, "McAdam");
-        Characters Ennemis = new Cargo(100,30,75,80, "Destructor");
 
-        Perso.attack(Ennemis);
+        View.cli.startMenu();
+        //Characters Perso = new Player(100, 200, 50, 50, "McAdam");
+        //Characters Ennemis = new Cargo(100,30,75,80, "Destructor");
+
+        //Perso.attack(Ennemis);
     }
 }
